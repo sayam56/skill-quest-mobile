440c721c43546720f5a24633961a0cd12b2ee94c
Ali Iktider <iktider@uwindsor.ca>
2023-07-22 17:11:41 -0400

    added duplicate files for sq-48

---
+++ lib/view/widgets/common/userProfileTile.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class UserProfileTile extends StatefulWidget {
+  final String cardText;
+  final int cardCount;
+
+  const UserProfileTile(this.cardText, this.cardCount, {super.key});
+
+  @override
+  State<UserProfileTile> createState() => _UserProfileTileState();
+}
+
+class _UserProfileTileState extends State<UserProfileTile> {
+  @override
+  Widget build(BuildContext context) {
+    String? cardText = widget.cardText;
+    int? cardCount = widget.cardCount;
+    return Container(
+      height: 100,
+      width: 170,
+      child: Padding(
+        padding: const EdgeInsets.all(5),
+        child: Card(
+          elevation: 15,
+          color: Colors.white,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    '$cardCount',
+                    style: const TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    cardText,
+                    style: const TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
